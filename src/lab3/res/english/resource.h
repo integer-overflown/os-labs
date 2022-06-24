1d68e98bdba32e32a46bf3fe67e4203d9e2c7b63
max-khm <maksym.khomenko@nure.ua>
2022-06-25 02:20:54 +0300

    + [Lab3]: Add resources in two languages

---
+++ src/lab3/res/english/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Lab3ResourcesEnglish.rc
+//
+#define ID_NAME                         101
+#define ID_GROUP                        102
+#define ID_FACULTY                      103
+#define ID_DISCIPLINE                   104
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
