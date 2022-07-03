c89d79a44efc64ddf43989f27ef89bfd8654b73f
max-khm <maksym.khomenko@nure.ua>
2022-07-03 14:11:55 +0300

    + [Lab2]: Add user input

--- src/lab2/main.cpp
+++ src/lab2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 
 #include "rsa.h"
 
@@ -10,8 +11,26 @@ int main() {
   PublicKey publicKey = cipher.publicKey();
   std::cout << "publicKey.n: " << publicKey.n << '\n';
 
-  uint64_t message = 8;
-  uint64_t enc = cipher.encrypt(message);
+  uint64_t message;
+  uint64_t enc;
+
+  for (;;) {
+    while (!((std::cout << '>' << ' ') && std::cin >> message)) {
+      std::cout << "Please enter a valid number" << '\n';
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    try {
+      enc = cipher.encrypt(message);
+      break;
+    } catch (const std::invalid_argument &ex) {
+      std::cout << "Unable to process given message, reason: " << ex.what();
+    } catch (const std::exception &ex) {
+      std::cout << "Unknown error occurred: " << ex.what();
+    }
+  }
+
   std::cout << "encode" << '(' << message << ')' << ':' << ' ' << enc << '\n';
   std::cout << "decode" << '(' << enc << ')' << ':' << ' '
             << cipher.decrypt(enc) << '\n';
