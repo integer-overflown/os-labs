32e894b557f84b8c09e0fc0a8cabd70dc538ccef
max-khm <maksym.khomenko@nure.ua>
2022-07-05 00:28:06 +0300

    + [Lab7]: Add impl for notepad launcher

---
+++ src/lab7/Task1/NotepadLauncher/main.cpp
@@ -0,0 +1,62 @@
+#include <tchar.h>
+#include <Windows.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#define CreateProcessSimple(launchLine, startUpInfo, processInfo)           \
+  CreateProcess(nullptr, (launchLine), nullptr, nullptr, false, 0, nullptr, \
+                nullptr, (startUpInfo), (processInfo))
+
+namespace lab7 {
+inline namespace literals {
+std::basic_string<TCHAR> operator""_ts(const TCHAR *string, size_t size) {
+  return {string, size};
+}
+}  // namespace literals
+
+#ifdef UNICODE
+constexpr auto cNotepadOpenMode = _T("/W");
+#else
+constexpr auto cNotepadOpenMode = _T("/A");
+#endif
+
+[[noreturn]] void HandleFatalWinApiError(const char *reason) {
+  std::cerr << reason << ", error code " << GetLastError() << '\n';
+  std::terminate();
+}
+
+}  // namespace lab7
+
+int main() {
+  using namespace lab7;
+  PROCESS_INFORMATION processInfo;
+  STARTUPINFO processStartUpInfo;
+
+  constexpr const TCHAR *cFileNames[] = {_T("first.txt"), _T("second.txt")};
+
+  SecureZeroMemory(&processInfo, sizeof(processInfo));
+  SecureZeroMemory(&processStartUpInfo, sizeof(processStartUpInfo));
+
+  processStartUpInfo.cb = sizeof(processStartUpInfo);
+  processStartUpInfo.dwFlags |= STARTF_USESTDHANDLES;
+
+  for (const TCHAR *fileName : cFileNames) {
+    auto launchLine = "notepad.exe"_ts + _T(' ') + lab7::cNotepadOpenMode +
+                      _T(' ') + fileName;
+
+    std::cout << "Launching process: " << launchLine << '\n';
+
+    if (!CreateProcessSimple(launchLine.data(), &processStartUpInfo,
+                             &processInfo)) {
+      HandleFatalWinApiError("CreateProcess failed");
+    }
+
+    if (WaitForSingleObject(processInfo.hProcess, INFINITE) == WAIT_FAILED) {
+      HandleFatalWinApiError("WaitForSingleObject failed");
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
