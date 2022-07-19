35c634df1d83cf2bcc40e26c7601e464147d8db0
max-khm <maksym.khomenko@nure.ua>
2022-07-19 23:38:19 +0300

    + [Lab7]: Task 3: Add measurements

--- src/lab7/Task3/main.cpp
+++ src/lab7/Task3/main.cpp
@@ -1,13 +1,37 @@
+#include <chrono>
 #include <iostream>
 
 #include "matrix.h"
 
+class ScopedObservation {
+  using ClockType = std::chrono::high_resolution_clock;
+
+ public:
+  ScopedObservation() : _startTime(ClockType::now()) {}
+
+  ~ScopedObservation() {
+    using namespace std::chrono;
+    std::cout
+        << duration_cast<milliseconds>(ClockType::now() - _startTime).count()
+        << "ms"
+        << '\n';
+  }
+
+ private:
+  std::chrono::time_point<ClockType> _startTime;
+};
+
 int main() {
-  lab7::Matrix m1(6, 10, [](size_t i, size_t j) { return float(i + j); });
-  lab7::Matrix identity(10, 1, [](size_t i, size_t j) { return float(i == j); });
+  lab7::Matrix m1(10, 100'000, [](size_t i, size_t j) { return float(i + j); });
+  lab7::Matrix identity(100'000, 8,[](size_t i, size_t j) { return float(i == j); });
 
-  std::cout << "m1:\n" << m1 << '\n';
-  std::cout << "identity:\n" << identity << '\n';
+  {
+    ScopedObservation observation;
+    m1.multiply(identity, std::execution::seq);
+  }
 
-  std::cout << "result:\n" << m1.multiply(identity, std::execution::par_unseq);
+  {
+    ScopedObservation observation;
+    m1.multiply(identity, std::execution::par_unseq);
+  }
 }
