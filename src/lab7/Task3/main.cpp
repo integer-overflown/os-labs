1188e803594859e68098683264866525f26dea9d
max-khm <maksym.khomenko@nure.ua>
2022-07-21 00:59:53 +0300

    + [Lab7]: Task3: Add small matrix test, add test labels

--- src/lab7/Task3/main.cpp
+++ src/lab7/Task3/main.cpp
@@ -13,8 +13,7 @@ class ScopedObservation {
     using namespace std::chrono;
     std::cout
         << duration_cast<milliseconds>(ClockType::now() - _startTime).count()
-        << "ms"
-        << '\n';
+        << "ms" << '\n';
   }
 
  private:
@@ -23,14 +22,26 @@ class ScopedObservation {
 
 int main() {
   lab7::Matrix m1(10, 100'000, [](size_t i, size_t j) { return float(i + j); });
-  lab7::Matrix identity(100'000, 8,[](size_t i, size_t j) { return float(i == j); });
+  lab7::Matrix identity(100'000, 8,
+                        [](size_t i, size_t j) { return float(i == j); });
+
+  lab7::Matrix smallMatrix1(
+      8, 8, [](size_t i, size_t j) { return float(2 * i + j); });
+  lab7::Matrix smallMatrix2(8, 8,
+                            [](size_t i, size_t j) { return float(int(i) - int(j)); });
+
+  std::cout << "smallMatrix1\n" << smallMatrix1 << '\n';
+  std::cout << "smallMatrix2\n" << smallMatrix2 << '\n';
+  std::cout << "smallMatrix1 x smallMatrix2 (sequential)\n" << smallMatrix1.multiply(smallMatrix2, std::execution::seq) << '\n';
 
   {
+    std::cout << "Large multiplication (sequential):\n";
     ScopedObservation observation;
     m1.multiply(identity, std::execution::seq);
   }
 
   {
+    std::cout << "Large multiplication (parallel):\n";
     ScopedObservation observation;
     m1.multiply(identity, std::execution::par_unseq);
   }
